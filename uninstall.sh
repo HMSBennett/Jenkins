91e715d3dca18fb802b92fde0a514e8c30e7bdd5
HMS <harrisonsbennett@gmail.com>
2019-06-21 10:01:11 +0100

    New file names for easier calling

--- uninstall.sh
+++
@@ -1,9 +0,0 @@
-sudo systemctl stop jenkins
-
-sudo apt-get remove --purge jenkins
-
-sudo userdel jenkins
-
-sudo rm -r /home/jenkins
-
-sudo rm /../../../../etc/systemd/system/jenkins.service
--- update.sh
+++
@@ -1,9 +0,0 @@
-sudo systemctl stop jenkins
-
-sudo su - jenkins -c "wget http://updates.jenkins-ci.org/latest/jenkins.war"
-
-sudo systemctl daemon-reload
-
-sudo systemctl start jenkins
-
-sudo systemctl status jenkins
